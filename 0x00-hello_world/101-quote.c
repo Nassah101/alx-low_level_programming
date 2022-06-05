22a2b2c4dd070dd447f110bfcc466e4d48f187a3
Hassan Wasiu <hassanwasiu00@gmail.com>
2022-06-05 04:11:51 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,6 @@
 #!/bin/bash
 #include <stdo.h>
 #include <unistd.h>
-   /**
-    *main - Entry point
-    *
-    *REturn: Always 0 (Success)
-    */
 int main(void)
 {
   Write(2, "and that piece of art is useful \" - Dora Korpar, 2015-1019\n",
