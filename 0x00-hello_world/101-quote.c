b08b0f165f57ee686389642581d081c353e7605f
Hassan Wasiu <hassanwasiu00@gmail.com>
2022-06-05 04:42:07 -0700

    whow

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdo.h>
 #include <unistd.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-  Write(2, "and that piece of art is useful \" - Dora Korpar, 2015-1019\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-  return (1);
+Write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
