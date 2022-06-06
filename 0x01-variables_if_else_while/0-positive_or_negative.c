9000faaa15002ba77161de2f1ff222462be8f89a
Hassan Wasiu <hassanwasiu00@gmail.com>
2022-06-06 03:57:57 -0700

    po

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-int main(void)
 /**
 * main - assign a random number to the variable n each time it is executed
 *if the number is greater than 0: is positive, if the number is 0: is zero
 *if the number is less than 0: is negative, followed by a new line
 *Retuen: 0
 */
+int main(void)
 {
 int n;
 srand(time(0));
