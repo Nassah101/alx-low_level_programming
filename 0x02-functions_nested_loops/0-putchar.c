3e83b09861bbd391ec34bc96962e4de3325db5a0
Hassan Wasiu <hassanwasiu00@gmail.com>
2022-06-08 00:55:58 -0700

    ok

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char *sh = "_putchar"
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
+}
