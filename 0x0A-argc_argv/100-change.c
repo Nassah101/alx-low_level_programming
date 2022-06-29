84d60754b5681d266272637aafe886307c5e58dc
Hassan Wasiu <hassanwasiu00@gmail.com>
2022-06-29 02:38:08 -0700

    m

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,9 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 /**
 * coin_cal - Helper function that does all the mathematics
 * @cents: int for number of cents
