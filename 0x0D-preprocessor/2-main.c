29eb5b2b600eeedd9f3d4aaba9f2c202a4df5c9c
Hassan Wasiu <hassanwasiu00@gmail.com>
2022-07-05 03:13:01 -0700

    op

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - program that prints out the name of the file being compiled
+* @void: Nothing
+* Return: success
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
